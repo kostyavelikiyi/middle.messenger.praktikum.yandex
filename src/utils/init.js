4075144959f04af3ac170a96348679de36eebf21
Великий Константин Андреевич <KVelikiy@alfabank.ru>
2023-09-03 09:09:23 +0300

    fix js import

--- src/utils/init.js
+++ src/utils/init.js
@@ -1,3 +1,19 @@
+class Auth {
+  constructor() {
+    const auth = localStorage.getItem("auth");
+    this.validateAuth(auth);
+  }
+  validateAuth(auth) {
+    if (auth != 1) {
+      window.location.replace("/pages/login/login.html");
+    }
+  }
+  logOut() {
+    localStorage.removeItem("auth");
+    window.location.replace("./pages/login/login.hbs");
+  }
+}
+
 const auth = new Auth();
 //todo перенести в отдельныый файл
 document.getElementById("logout").addEventListener("click", (e) => {
@@ -5,5 +21,3 @@ document.getElementById("logout").addEventListener("click", (e) => {
   console.log("logout");
   auth.logOut();
 });
-
-
