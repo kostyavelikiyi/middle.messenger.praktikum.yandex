7ed2d6d4c89d6859e7d800eddadae59c30b8707a
Великий Константин Андреевич <KVelikiy@alfabank.ru>
2023-09-03 08:47:28 +0300

    hndlebar work on server

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-  plugins: [handlebars()],
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src/partials"),
+      context: {
+        username: "Hello, world!",
+      },
+    }),
+  ],
   root: "src",
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        login: resolve(__dirname, "src/pages/login/login.html"),
+      },
+    },
   },
 });
