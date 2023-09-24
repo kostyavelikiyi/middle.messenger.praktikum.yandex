ae2d0a6ca3677a2b292e34afb6dc88d8f683e651
Великий Константин Андреевич <KVelikiy@alfabank.ru>
2023-09-24 23:32:29 +0300

    fix errors

--- src/components/form_auth/index.ts
+++ src/components/form_auth/index.ts
@@ -1,2 +1,5 @@
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import './form_auth.css';
+// @ts-ignore
 export { default as FormAuth } from './form_auth.hbs?raw';
--- src/components/profile_data_container/index.ts
+++ src/components/profile_data_container/index.ts
@@ -1,2 +1,5 @@
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import './profile_data_container.css';
+// @ts-ignore
 export { default as ProfileDataContainer } from './profile_data_container.hbs?raw';
