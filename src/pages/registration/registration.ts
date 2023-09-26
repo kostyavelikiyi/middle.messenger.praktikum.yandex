6a8d69aae5011b522247b5c9048c2167b8b5d859
Великий Константин Андреевич <KVelikiy@alfabank.ru>
2023-09-26 23:48:23 +0300

    added validation on input on registration page

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -4,6 +4,21 @@ export class RegistrationPage extends Block {
   constructor() {
     super({
       validate: {
+        email: (value: string) => {
+          let error = '';
+          if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
+            error = 'невалидный email';
+          }
+          return error;
+        },
+        name: (value: string) => {
+          let error = '';
+          if (!/^[A-ZА-ЯЁ][а-яА-ЯёЁ-]+$/.test(value)) {
+            error =
+              'латиница или кириллица, первая буква должна быть заглавной, без пробелов и без цифр, нет спецсимволов (кроме -)';
+          }
+          return error;
+        },
         login: (value: string) => {
           let error = '';
           if (!/^(?=.*[A-Za-z])[A-Za-z0-9_-]{3,20}$/.test(value)) {
@@ -12,11 +27,20 @@ export class RegistrationPage extends Block {
           }
           return error;
         },
+        phone: (value: string) => {
+          let error = '';
+          if (
+            !/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,15}(\s*)?$/.test(value)
+          ) {
+            error = 'неверный номер телефона';
+          }
+          return error;
+        },
         password: (value: string) => {
           let error = '';
           if (!/^(?=.*?[A-Z])(?=.*?[0-9]).{8,40}$/.test(value)) {
             error =
-              'от 3 до 20 символов, латиница, может содержать цифры, но не состоять из них, без пробелов, без спецсимволов (допустимы дефис и нижнее подчёркивание)';
+              'от 8 до 40 символов, обязательно хотя бы одна заглавная буква и цифра';
           }
           return error;
         },
@@ -25,8 +49,8 @@ export class RegistrationPage extends Block {
         event.preventDefault();
         const email = this.refs.email.value();
         const login = this.refs.login.value();
-        const name = this.refs.name.value();
-        const lastName = this.refs.lastName.value();
+        const name = this.refs.first_name.value();
+        const lastName = this.refs.last_name.value();
         const phone = this.refs.phone.value();
         const password = this.refs.password.value();
         const password2 = this.refs.password2.value();
@@ -50,8 +74,8 @@ export class RegistrationPage extends Block {
                 {{#> FormAuth}}
                     {{{ InputField label="Почта" ref="email" validate=validate.email }}}
                     {{{ InputField label="Логин" ref="login" validate=validate.login }}}
-                    {{{ InputField label="Имя" ref="name" validate=validate.name }}}
-                    {{{ InputField label="Фамилия" ref="lastName" validate=validate.lastName }}}
+                    {{{ InputField label="Имя" ref="first_name" validate=validate.name }}}
+                    {{{ InputField label="Фамилия" ref="last_name" validate=validate.name }}}
                     {{{ InputField label="Телфон" ref="phone" validate=validate.phone }}}
                     {{{ InputField label="Пароль" ref="password" validate=validate.password }}}
                     {{{ InputField label="Пароль (еще раз)" ref="password2" validate=validate.password }}}
